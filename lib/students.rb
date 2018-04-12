d08c78837a4e0b48e38d9ff47047a685cca89be5
Zac Willmington <zac.willmington@gmail.com>
2018-04-12 16:36:48 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,4 +1,4 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
     "SELECT * FROM #{@db} ORDER BY gpa LIMIT 1;"
